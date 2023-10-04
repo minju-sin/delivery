4b066d2974249e744285d6ea39d1c24ab9796eca
minju-sin <sinminju04@gmail.com>
2023-10-04 16:23:35 +0900

    회원가입 아이디(학번) 중복 사용자 체크
    화면에서도 가입 버튼을 누르면 학번 중복이 체크된다.

--- src/main/java/com/delivery/controller/UserController.java
+++ src/main/java/com/delivery/controller/UserController.java
@@ -2,21 +2,30 @@ package com.delivery.controller;
 
 import com.delivery.domain.User;
 import com.delivery.service.UserService;
-import lombok.Getter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // 해당 클래스가 컨트롤러임을 알리고 bean으로 등록하기 위함
 @RequiredArgsConstructor // 의존관계 관련해 필요한 어노테이션
 public class UserController {
     @Autowired
     private UserService userService;
-    
+
     // 메인 페이지
     @GetMapping("/")
     public String Home(){
@@ -27,14 +36,22 @@ public class UserController {
     @GetMapping("/signup")
     public String dispSignUp(Model model){
         model.addAttribute("user", new User());
+
         return "/signup";
     }
 
+
     // 회원가입 처리
     @PostMapping("/signup")
-    public String execSignUp(@ModelAttribute("user") User user){
-        userService.signupUser(user);
-        return "redirect:/";
+    public String execSignUp(@ModelAttribute("user") User user, Model model) {
+        if (userService.isUserIdUnique(user.getUserId())) {
+            userService.signupUser(user);
+            return "redirect:/"; // 가입 성공 시 홈 페이지로 리다이렉션
+        } else {
+            // 중복된 아이디일 경우 에러 메시지를 표시하고 회원 가입 페이지로 다시 이동
+            model.addAttribute("userExistsMsg", "이미 사용 중인 아이디입니다.");
+            return "signup";
+        }
     }
 
 
@@ -44,11 +61,6 @@ public class UserController {
         return "/login";
     }
 
-    // 접근 거부 페이지
-    @GetMapping("/user/denied")
-    public String Denied(){
-        return "/denied";
-    }
 
     // 내 정보 페이지
     @GetMapping("/user/info")
--- src/main/java/com/delivery/service/UserService.java
+++ src/main/java/com/delivery/service/UserService.java
@@ -12,12 +12,19 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    public boolean isUserIdUnique(String userId) {
+        User existingUser = userRepository.findByUserId(userId);
+        return existingUser == null;
+    }
 
     public void signupUser(User user){
         //  사용자에게 기본적으로 일반 사용자 권한 할당
         user.setRole(UserRole.USER);
+
+        // 중복 사용자 ID 체크
+        if (!isUserIdUnique(user.getUserId())) {
+            throw new RuntimeException("사용자 ID가 이미 존재합니다.");
+        }
 
         // 사용자 저장
         userRepository.save(user);
