db1949cc919e6758ebf729d14f25de3e470d0e54
minju-sin <sinminju04@gmail.com>
2023-10-05 21:06:23 +0900

    기본적인 로그인, 로그아웃 구현 성공

--- src/main/java/com/delivery/controller/UserController.java
+++ src/main/java/com/delivery/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.delivery.controller;
 
+import com.delivery.domain.User;
 import com.delivery.dto.UserDTO;
+import com.delivery.repository.UserRepository;
 import com.delivery.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,25 +13,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /*
  * 메인 페이지 /home
- * 로그인 페이지 /login
  * 회원가입 페이지 /signup
+ * 로그인 페이지 /login
+ * 로그아웃 페이지 /logout
  * 관리자 페이지 /admin
  * */
 
 @Controller // 해당 클래스가 컨트롤러임을 알리고 bean으로 등록하기 위함
 @RequiredArgsConstructor // 의존관계 관련해 필요한 어노테이션
 public class UserController {
-    @Autowired
-    private UserService userService; // 사용자 서비스 (데이터베이스에서 사용자 정보를 가져옴)
+    @Autowired private UserService userService; // 사용자 서비스 (데이터베이스에서 사용자 정보를 가져옴)
+    @Autowired private UserRepository userRepository;   //  데이터베이스에 저장된 아이디 가져옴
 
     // 메인 페이지
     @GetMapping("/")
-    public String Home(){
-        return "/home";
+    public String Home(HttpSession session) {
+        // 세션에서 로그인 상태를 확인
+        boolean isLoggedIn = session.getAttribute("isLoggedIn") != null && (boolean) session.getAttribute("isLoggedIn");
+
+        return isLoggedIn ? "/home_logged_in" : "/home_not_logged_in";
     }
+
 
     // 회원가입 페이지
     @GetMapping("/signup")
@@ -58,11 +68,33 @@ public class UserController {
         return "/login";
     }
 
+
+    // 로그인 처리
     @PostMapping("/login")
-    public String login() {
-        // 사용자 로그인 로직을 구현
-        // 스프링 시큐리티를 사용하여 로그인 처리
-        return "redirect:/"; // 로그인 성공 시 홈 페이지로 리디렉션
+    public String login(@RequestParam("userId") String userId,
+                        @RequestParam("password") String password, Model model) {
+        User user = userRepository.findByUserId(userId);
+
+        if (user != null && user.getPassword().equals(password)) {
+            // 로그인 성공
+            return "/home_logged_in"; // 로그인 후 이동할 페이지
+        } else {
+            // 로그인 실패
+            model.addAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다.");
+            return "/login";
+        }
+    }
+
+    // 로그아웃 처리
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        // 세션을 만료시킴
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return "redirect:/"; // 로그아웃 후 메인 페이지로 리다이렉트
     }
 
     /*
