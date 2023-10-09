c46ae83a3d395a4f5ad748d009b552f361ad65bf
minju-sin <sinminju04@gmail.com>
2023-10-09 14:52:48 +0900

    sql수정
    메인화

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO User (user_id, username, department, password, phone, address, details_address, role)
-VALUES ('admin', '관리자', 'Admin Department', 'password1234', '010-1234-5678', '부산광역시 부산진구', '동의대학교', 'ADMIN');
+VALUES ('admin', '관리자', 'Admin Department', 'Password1234@#', '010-1234-5678', '부산광역시 부산진구', '동의대학교', 'ADMIN');
